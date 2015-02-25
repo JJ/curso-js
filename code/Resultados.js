5ead910ed4d727b1c25902995a95e6cc7f68a4a7
JJ Merelo <jjmerelo@gmail.com>
2015-02-25 08:10:22 +0100

    Revisado formato
    Quería solucionar #24 pero la verdad no sé a qué programa o texto te refieres. La quiniela se usa como ejemplo siempre.

--- code/Resultados.js
+++ code/Resultados.js
@@ -1,45 +1,44 @@
-// ejecutar con rhino lee_quiniela.js <argumento>
-
+// Lee los resultados de un fichero y los devuelve como array.
 function lee_resultados( file_name ) {
-  var FileReader = java.io.FileReader;
-  var BufferedReader =java.io.BufferedReader;
-  var f = new FileReader(file_name);
-  var br = new BufferedReader( f );
-  var resultados= new Array;
-  var line = new String;
-  while ((line = br.readLine()) != null) {
-    var estaLinea = new String( line );
-    var resultado  = estaLinea.split(" ");
-    switch (resultado[2]) {
-    case '1':
-      if ( resultados[resultado[0]] ) {
-	resultados[resultado[0]]+=3;
-      } else {
-	resultados[resultado[0]]=3;
-      }
-      break;
-    case 'x':
-      if ( resultados[resultado[0]] ) {
-	resultados[resultado[0]]+=1;
-      } else {
-	resultados[resultado[0]]=1;
-      }
-      if ( resultados[resultado[1]] ) {
-	resultados[resultado[1]]+=1;
-      } else {
-	resultados[resultado[1]]=1;
-      }
-      break;
-    default:
-      if ( resultados[resultado[1]] ) {
-	resultados[resultado[1]]+=3;
-      } else {
-	resultados[resultado[1]]=3;
-      }
-      break
+    var FileReader = java.io.FileReader;
+    var BufferedReader =java.io.BufferedReader;
+    var f = new FileReader(file_name);
+    var br = new BufferedReader( f );
+    var resultados= new Array;
+    var line = new String;
+    while ((line = br.readLine()) != null) {
+	var estaLinea = new String( line );
+	var resultado  = estaLinea.split(" ");
+	switch (resultado[2]) {
+	case '1':
+	    if ( resultados[resultado[0]] ) {
+		resultados[resultado[0]]+=3;
+	    } else {
+		resultados[resultado[0]]=3;
+	    }
+	    break;
+	case 'x':
+	    if ( resultados[resultado[0]] ) {
+		resultados[resultado[0]]+=1;
+	    } else {
+		resultados[resultado[0]]=1;
+	    }
+	    if ( resultados[resultado[1]] ) {
+		resultados[resultado[1]]+=1;
+	    } else {
+		resultados[resultado[1]]=1;
+	    }
+	    break;
+	default:
+	    if ( resultados[resultado[1]] ) {
+		resultados[resultado[1]]+=3;
+	    } else {
+		resultados[resultado[1]]=3;
+	    }
+	    break
 	}
-  }
+    }
   
-  return resultados;
+    return resultados;
 }
 
--- code/lee_quiniela.js
+++ code/lee_quiniela.js
@@ -10,40 +10,41 @@ var br = new BufferedReader( f );
 var resultados= new Array;
 var line = new String;
 while ((line = br.readLine()) != null) {
-  var estaLinea = new String( line );
-  var resultado  = estaLinea.split(" ");
-  switch (resultado[2]) {
+    var estaLinea = new String( line );
+    var resultado  = estaLinea.split(" ");
+ 
+    switch (resultado[2]) {
     case '1':
-      if ( resultados[resultado[0]] ) {
-	resultados[resultado[0]]+=3;
-      } else {
-	resultados[resultado[0]]=3;
-      }
-      break;
+	if ( resultados[resultado[0]] ) {
+	    resultados[resultado[0]]+=3;
+	} else {
+	    resultados[resultado[0]]=3;
+	}
+	break;
     case 'x':
-      if ( resultados[resultado[0]] ) {
-	resultados[resultado[0]]+=1;
-      } else {
-	resultados[resultado[0]]=1;
-      }
-      if ( resultados[resultado[1]] ) {
-	resultados[resultado[1]]+=1;
-      } else {
-	resultados[resultado[1]]=1;
-      }
-      break;
+	if ( resultados[resultado[0]] ) {
+	    resultados[resultado[0]]+=1;
+	} else {
+	    resultados[resultado[0]]=1;
+	}
+	if ( resultados[resultado[1]] ) {
+	    resultados[resultado[1]]+=1;
+	} else {
+	    resultados[resultado[1]]=1;
+	}
+	break;
     default:
-       if ( resultados[resultado[1]] ) {
-	resultados[resultado[1]]+=3;
-      } else {
-	resultados[resultado[1]]=3;
-      }
-      break
-  }
- }
+	if ( resultados[resultado[1]] ) {
+	    resultados[resultado[1]]+=3;
+	} else {
+	    resultados[resultado[1]]=3;
+	}
+	break
+    }
+}
 
 for ( var i in resultados ) {
-  print( i + ": " + resultados[i])
-    }
+    print( i + ": " + resultados[i])
+}
 
 
