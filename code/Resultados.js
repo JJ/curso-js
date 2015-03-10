0c3faeed109f07d6ddb55050c8b007af27f4660b
Jose Antonio Vacas <javacasm@gmail.com>
2015-03-10 18:07:17 +0100

    Smaller code
    Smaller code, just making the checking before the switch

--- code/Resultados.js
+++ code/Resultados.js
@@ -6,35 +6,25 @@ function lee_resultados( file_name ) {
     var br = new BufferedReader( f );
     var resultados= new Array;
     var line = new String;
+    if ( !resultados[resultado[0]] ) {
+    	resultados[resultado[0]]=0;
+    }
+    if ( !resultados[resultado[1]] ) {
+	resultados[resultado[1]]=0;
+    }
     while ((line = br.readLine()) != null) {
 	var estaLinea = new String( line );
 	var resultado  = estaLinea.split(" ");
 	switch (resultado[2]) {
 	case '1':
-	    if ( resultados[resultado[0]] ) {
-		resultados[resultado[0]]+=3;
-	    } else {
-		resultados[resultado[0]]=3;
-	    }
+	    resultados[resultado[0]]+=3;
 	    break;
 	case 'x':
-	    if ( resultados[resultado[0]] ) {
-		resultados[resultado[0]]+=1;
-	    } else {
-		resultados[resultado[0]]=1;
-	    }
-	    if ( resultados[resultado[1]] ) {
-		resultados[resultado[1]]+=1;
-	    } else {
-		resultados[resultado[1]]=1;
-	    }
+	    resultados[resultado[0]]+=1;
+	    resultados[resultado[1]]+=1;
 	    break;
 	default:
-	    if ( resultados[resultado[1]] ) {
-		resultados[resultado[1]]+=3;
-	    } else {
-		resultados[resultado[1]]=3;
-	    }
+	    resultados[resultado[1]]+=3;
 	    break
 	}
     }
